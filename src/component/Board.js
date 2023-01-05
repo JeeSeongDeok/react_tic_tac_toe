4fc85120ec05ddead87913ddfd91166dc2f8f245
unknown <bh939@naver.com>
2023-01-05 10:35:34 +0900

    state 올리기

--- src/component/Board.js
+++ src/component/Board.js
@@ -2,8 +2,24 @@ import React from "react";
 import Square from "./Square"
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice;
+    squares[i] = 'X';
+    this.setState({ squares: squares });
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square
+      value={this.state.squares[i]}
+      onClick={() => this.handleClick(i)}
+    />;
   }
 
   render() {
--- src/component/Square.js
+++ src/component/Square.js
@@ -1,17 +1,13 @@
 import React from 'react';
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
 
   render() {
     return (
-      <button className="square" onClick={() => { this.setState({ value: 'X' }) }}>
-        {this.state.value}
+      <button
+        className="square"
+        onClick={() => { this.props.onClick() }}>
+        {this.props.value}
       </button>
     );
   }
