94dc72c1741ba9f879f1b71ccada4a06f6e2b155
unknown <bh939@naver.com>
2023-01-05 11:04:36 +0900

    틱택톡 순서 정하기

--- src/component/Board.js
+++ src/component/Board.js
@@ -6,13 +6,17 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
   handleClick(i) {
-    const squares = this.state.squares.slice;
-    squares[i] = 'X';
-    this.setState({ squares: squares });
+    const squares = this.state.squares.slice();
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -23,7 +27,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
--- src/component/Square.js
+++ src/component/Square.js
@@ -1,16 +1,11 @@
 import React from 'react';
 
-class Square extends React.Component {
-
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={() => { this.props.onClick() }}>
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className='square' onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 export default Square
