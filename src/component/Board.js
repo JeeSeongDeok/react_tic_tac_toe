77da662283a5e715dd7acc84977d169be0acb816
unknown <bh939@naver.com>
2023-01-05 11:21:21 +0900

    틱택톡 승리 조건 추가

--- src/component/Board.js
+++ src/component/Board.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Square from "./Square"
+import calculateWinner from "../model/calculateWinner";
 
 class Board extends React.Component {
   constructor(props) {
@@ -12,6 +13,9 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
@@ -27,7 +31,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
