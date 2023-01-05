194f5ac2b4cf5771535276fee03e1eaace6cd7ec
unknown <bh939@naver.com>
2023-01-05 09:40:24 +0900

    틱택톡 보드판 추가

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Game from './component/Game'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
